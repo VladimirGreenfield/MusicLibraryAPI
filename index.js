cd8d8708c3710c9d09e7cee75d697e8d1ba007d6
vladimir greenfield <vladdygreenfield@gmail.com>
2021-11-23 00:42:01 -0500

    created app.get methods

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const repoContext = require('./repository/repository-wrapper');
+
+
 const app = express();
+
+app.get('/api/songs', (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+});
+
+app.get('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const songs = repoContext.songs.findSongById(id);
+    return res.send(songs);
+});
+
 
 app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
